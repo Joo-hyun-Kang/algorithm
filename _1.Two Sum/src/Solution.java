857ef48f548b95cc580898300fc32a04bc98b65b
Joo-hyun-Kang <autoba9687@gmail.com>
2024-02-17 00:13:23 +0900

    feat: improve O(n) by hashmap

--- _1.Two Sum/src/Solution.java
+++ _1.Two Sum/src/Solution.java
@@ -1,19 +1,29 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         // nums = [2,7,11,15], target = 9
+        // nums = [3, 3], target = 6
+        HashMap<Integer, Integer> map = new HashMap();
 
+        for (int i = 0; i < nums.length; i++) {
+            map.put(i, nums[i]);
+        }
+        
         int index1 = 0;
         int index2 = 0;
-        
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (target == nums[i] + nums[j])    {
-                    index1 = i;
-                    index2 = j;
+            int findValue = target - nums[i];
+            if (map.containsValue(findValue)) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (target == nums[i] + nums[j]) {
+                        index1 = i;
+                        index2 = j;
+                    }
                 }
             }
         }
 
-        return new int[]{index1, index2};
+        return new int[] {index1, index2};
     }
 }
