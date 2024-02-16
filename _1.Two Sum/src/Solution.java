bf254f6f00eb3946e6af01555d7220ad0e661bf4
Joohyun Kang <jokang@c1r11s8.42tokyo.jp>
2024-02-16 23:49:27 +0900

    feat: solving by simple brute-force

--- _1.Two Sum/src/Solution.java
+++ _1.Two Sum/src/Solution.java
@@ -1,8 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // nums = [2,7,11,15], target = 9
 
+        int index1 = 0;
+        int index2 = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j])    {
+                    index1 = i;
+                    index2 = j;
+                }
+            }
+        }
 
-
-        return null;
+        return new int[]{index1, index2};
     }
 }
