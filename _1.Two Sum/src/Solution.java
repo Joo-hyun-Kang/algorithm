c2f304becff0e777768e65e838d7b76cd4eb6947
Joo-hyun-Kang <autoba9687@gmail.com>
2024-02-17 21:15:44 +0900

    feat: make to use HashMap more effiency

--- _1.Two Sum/src/Solution.java
+++ _1.Two Sum/src/Solution.java
@@ -4,26 +4,15 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         // nums = [2,7,11,15], target = 9
         // nums = [3, 3], target = 6
-        HashMap<Integer, Integer> map = new HashMap();
-
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(i, nums[i]);
-        }
-        
-        int index1 = 0;
-        int index2 = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int findValue = target - nums[i];
-            if (map.containsValue(findValue)) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (target == nums[i] + nums[j]) {
-                        index1 = i;
-                        index2 = j;
-                    }
-                }
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
             }
+            map.put(nums[i], i);
         }
 
-        return new int[] {index1, index2};
+        return new int[] {-1, -1};
     }
 }
