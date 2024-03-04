242bf5d552fd581d48b972dea37aad965b7d6b23
Joo-hyun-Kang <autoba9687@gmail.com>
2024-03-04 19:07:30 +0900

    fix: remove overlap in 1st nodes

--- 場合の数/_47.Permutation2/src/Solution.java
+++ 場合の数/_47.Permutation2/src/Solution.java
@@ -4,16 +4,15 @@ class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> element = new ArrayList<>();
-        boolean[] isOverlap = new boolean[nums.length];
+        HashSet<Integer> isWidthOverlap = new HashSet<>();
+        HashSet<Integer> isStemOverlap = new HashSet<>();
 
-        Arrays.sort(nums);
-
-        doBacktrack(nums, isOverlap, res, element);
+        doBacktrack(nums, isWidthOverlap, isStemOverlap, res, element);
 
         return res;
     }
 
-    private void doBacktrack(int[] nums, boolean[] isOverlap, List<List<Integer>> res, List<Integer> element) {
+    private void doBacktrack(int[] nums, HashSet<Integer> isWidthOverlap, HashSet<Integer> isStemOverlap, List<List<Integer>> res, List<Integer> element) {
         // { 1, 1, 3, 4}
         if (element.size() == nums.length) {
             res.add(new ArrayList<>(element));
@@ -21,22 +20,28 @@ class Solution {
         }
 
         for (int i = 0; i < nums.length; i++) {
-//            int[] nums = {1, 1, 2, 3};
-            if (isOverlap[i] == true) {
+//            int[] nums = {1, 2, 2, 3};
+//            3 2 2 1
+//            int[] nums = {1, 1, 3};
+            if (isWidthOverlap.size() == 0 && isStemOverlap.contains(nums[i])) {
                 continue;
             }
 
-            if (i > 0 && nums[i - 1] == nums[i]) {
+            if (isWidthOverlap.size() == 0) {
+                isStemOverlap.add(nums[i]);
+            }
+
+            if (isWidthOverlap.contains(i) ) {
                 continue;
             }
 
             element.add(nums[i]);
-            isOverlap[i] = true;
+            isWidthOverlap.add(i);
 
-            doBacktrack(nums, isOverlap, res, element);
+            doBacktrack(nums, isWidthOverlap, isStemOverlap, res, element);
 
             element.remove(element.size() - 1);
-            isOverlap[i] = false;
+            isWidthOverlap.remove(i);
         }
     }
 }
