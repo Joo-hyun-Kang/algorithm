7820cabdc1053af07d7248f356aae80b4cf2c6dd
Joo-hyun-Kang <autoba9687@gmail.com>
2024-03-05 17:15:28 +0900

    fix: check start point for tree of deptps

--- 場合の数/_47.Permutation2/src/Main.java
+++ 場合の数/_47.Permutation2/src/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] nums = {1, 1, 3};
+        int[] nums = {1, 2, 2, 3};
 
         List<List<Integer>> lists = sol.permuteUnique(nums);
         for (List<Integer> list : lists) {
--- 場合の数/_47.Permutation2/src/Solution.java
+++ 場合の数/_47.Permutation2/src/Solution.java
@@ -7,41 +7,45 @@ class Solution {
         HashSet<Integer> isWidthOverlap = new HashSet<>();
         HashSet<Integer> isStemOverlap = new HashSet<>();
 
-        doBacktrack(nums, isWidthOverlap, isStemOverlap, res, element);
+        Arrays.sort(nums);
+
+        doBacktrack(0, nums, isWidthOverlap, isStemOverlap, res, element);
 
         return res;
     }
 
-    private void doBacktrack(int[] nums, HashSet<Integer> isWidthOverlap, HashSet<Integer> isStemOverlap, List<List<Integer>> res, List<Integer> element) {
-        // { 1, 1, 3, 4}
+    private void doBacktrack(int start, int[] nums, HashSet<Integer> isWidthOverlap, HashSet<Integer> isStemOverlap, List<List<Integer>> res, List<Integer> element) {
         if (element.size() == nums.length) {
             res.add(new ArrayList<>(element));
             return ;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-//            int[] nums = {1, 2, 2, 3};
-//            3 2 2 1
-//            int[] nums = {1, 1, 3};
-            if (isWidthOverlap.size() == 0 && isStemOverlap.contains(nums[i])) {
+        // 1 2 2 2 3
+        //
+        for (int index = 0; index < nums.length; index++)
+        {
+            //index %= nums.length;
+
+            if (index > start && nums[index - 1] == nums[index]) {
+                index++;
                 continue;
             }
 
-            if (isWidthOverlap.size() == 0) {
-                isStemOverlap.add(nums[i]);
-            }
-
-            if (isWidthOverlap.contains(i) ) {
+            if (isWidthOverlap.contains(index) ) {
+                index++;
                 continue;
             }
 
-            element.add(nums[i]);
-            isWidthOverlap.add(i);
+            element.add(nums[index]);
+            isWidthOverlap.add(index);
 
-            doBacktrack(nums, isWidthOverlap, isStemOverlap, res, element);
+            // 課程
+            doBacktrack(index + 1, nums, isWidthOverlap, isStemOverlap, res, element);
 
             element.remove(element.size() - 1);
-            isWidthOverlap.remove(i);
+            isWidthOverlap.remove(index);
+
+            index++;
         }
     }
 }
