b962fdc8a678a80cf26bca6b3ebe3731907c9daf
Joo-hyun-Kang <autoba9687@gmail.com>
2024-03-06 15:54:09 +0900

    fix: removing unnecessary code

--- 場合の数/_47.Permutation2/src/Solution.java
+++ 場合の数/_47.Permutation2/src/Solution.java
@@ -4,17 +4,16 @@ class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> element = new ArrayList<>();
-        HashSet<Integer> isWidthOverlap = new HashSet<>();
-        HashSet<Integer> isStemOverlap = new HashSet<>();
 
+        HashSet<Integer> isWidthOverlap = new HashSet<>();
         Arrays.sort(nums);
 
-        doBacktrack(nums, isWidthOverlap, isStemOverlap, res, element);
+        doBacktrack(nums, isWidthOverlap, res, element);
 
         return res;
     }
 
-    private void doBacktrack(int[] nums, HashSet<Integer> isWidthOverlap, HashSet<Integer> isStemOverlap, List<List<Integer>> res, List<Integer> element) {
+    private void doBacktrack(int[] nums, HashSet<Integer> isWidthOverlap, List<List<Integer>> res, List<Integer> element) {
         if (element.size() == nums.length) {
             res.add(new ArrayList<>(element));
             return ;
@@ -22,18 +21,14 @@ class Solution {
 
         // 1 2 2 2 3
         boolean isStartOn = false;
-        int start = -1;
         for (int index = 0; index < nums.length; index++)
         {
             if (isWidthOverlap.contains(index)) {
                 continue;
-            } else if (!isStartOn) {
-                start = index;
-                isStartOn = true;
             }
 
             // 含めている？　ー＞　!isWidthOverlap.contains(index - 1)　
-            if (isStartOn && index > start && !isWidthOverlap.contains(index - 1)) {
+            if (index > 0 && !isWidthOverlap.contains(index - 1)) {
                 if (nums[index - 1] == nums[index]) {
                     continue;
                 }
@@ -44,7 +39,7 @@ class Solution {
 
             // startはすすんてる　→ 事実上のstartポイント　伝えば
             // こっちでfor loop?
-            doBacktrack(nums, isWidthOverlap, isStemOverlap, res, element);
+            doBacktrack(nums, isWidthOverlap, res, element);
 
             element.remove(element.size() - 1);
             isWidthOverlap.remove(index);
