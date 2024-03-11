5efec0c722b4c8f8f488ea9a897db3cbb15713fc
Joo-hyun-Kang <autoba9687@gmail.com>
2024-03-11 11:18:13 +0900

    feat: sloving by recursive

--- _1009. Complement of Base 10 Integer/src/Solution.java
+++ _1009. Complement of Base 10 Integer/src/Solution.java
@@ -4,21 +4,42 @@ class Solution {
         // nの２進法の補数
         //　10 => 1010　すぐ　0101　変換
         // 3
-        // 0101
-        //
+        // 0011
+        // 0000
 
-        int copy = n;
-        int res = 0;
-        while (copy > 0) {
-            if (copy % 2 == 0) {
-                // 10
-                // 5
-                // 0 ->
-            }
-
-            copy /= 2;
-        }
+        // 5
+        // 101
+        // 010
+        int res = recursive(n,0);
 
         return -1;
     }
+
+    public int recursive(int value, int depths) {
+        if (value == 0) {
+            return 0;
+        }
+
+        int res = recursive(value / 2, depths + 1);
+
+        if (value % 2 == 0) {
+            // 多分いらない
+            res += 0;
+        } else {
+            res += 2 * depths;
+        }
+
+        return res;
+    }
+
+    // 1 -> 1 -> 0
+    // 0 -> 0 -> 1
+
+    // 2 -> 10 -> 01
+    // 3 -> 11 -> 00
+
+    // 4 -> 100 -> 011
+    //     3210
+    //     0124
+
 }
