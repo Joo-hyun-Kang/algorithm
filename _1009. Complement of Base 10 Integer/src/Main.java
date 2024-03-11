9b20eba332620ccdb59263119dba91a1d53dec89
Joo-hyun-Kang <autoba9687@gmail.com>
2024-03-11 11:53:23 +0900

    fix: getting number position value

--- _1009. Complement of Base 10 Integer/src/Main.java
+++ _1009. Complement of Base 10 Integer/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solution sol = new Solution();
+        System.out.println(sol.bitwiseComplement(3));
+
+        // 1 -> 1 -> 0
+        // 0 -> 0 -> 1
+
+        // 2 -> 10 -> 01
+        // 3 -> 11 -> 00
+
+        // 4 -> 100 -> 011
+        //     3210
+        //     0124
+
     }
 }
--- _1009. Complement of Base 10 Integer/src/Solution.java
+++ _1009. Complement of Base 10 Integer/src/Solution.java
@@ -1,45 +1,28 @@
 class Solution {
     public int bitwiseComplement(int n) {
-
-        // nの２進法の補数
-        //　10 => 1010　すぐ　0101　変換
-        // 3
-        // 0011
-        // 0000
-
-        // 5
-        // 101
-        // 010
-        int res = recursive(n,0);
-
-        return -1;
+        return recursive(n,0);
     }
 
-    public int recursive(int value, int depths) {
+    private int recursive(int value, int depths) {
         if (value == 0) {
             return 0;
         }
 
         int res = recursive(value / 2, depths + 1);
 
+        // 現時点の数位置の値を知る様にする
+        int posValue = 1;
+        for (int i = 0; i < depths; i++) {
+            posValue *= 2;
+        }
+
         if (value % 2 == 0) {
-            // 多分いらない
-            res += 0;
-        } else {
-            res += 2 * depths;
+            res += posValue;
         }
 
         return res;
     }
 
-    // 1 -> 1 -> 0
-    // 0 -> 0 -> 1
 
-    // 2 -> 10 -> 01
-    // 3 -> 11 -> 00
-
-    // 4 -> 100 -> 011
-    //     3210
-    //     0124
 
 }
