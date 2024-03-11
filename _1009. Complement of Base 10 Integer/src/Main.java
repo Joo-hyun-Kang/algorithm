0b73dbe863389ce1ff51122b6aaa6829ac251ce8
Joo-hyun-Kang <autoba9687@gmail.com>
2024-03-11 12:06:15 +0900

    chore: clean up unnessarry things

--- _1009. Complement of Base 10 Integer/src/Main.java
+++ _1009. Complement of Base 10 Integer/src/Main.java
@@ -1,17 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.bitwiseComplement(3));
-
-        // 1 -> 1 -> 0
-        // 0 -> 0 -> 1
-
-        // 2 -> 10 -> 01
-        // 3 -> 11 -> 00
-
-        // 4 -> 100 -> 011
-        //     3210
-        //     0124
-
     }
 }
