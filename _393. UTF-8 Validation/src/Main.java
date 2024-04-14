e38cae60e7c07d97d5f4a43c19a193c1a988416e
Joo-hyun-Kang <autoba9687@gmail.com>
2024-04-14 15:46:47 +0900

    fix: change loop for debug

--- _393. UTF-8 Validation/src/Main.java
+++ _393. UTF-8 Validation/src/Main.java
@@ -3,11 +3,19 @@ public class Main {
 
         Solution s = new Solution();
 
-        int[] array1 = {197, 130, 1};
+        int[] array1 = {255};
         System.out.println(s.validUtf8(array1));
 
-        int[] array2 = {230,136,145};
+        int[] array2 = {250,145,145,145,145};
         System.out.println(s.validUtf8(array2));
 
+        array1 = new int[]{197, 130, 1};
+        System.out.println(s.validUtf8(array1));
+
+        array1 = new int[]{235, 140, 4};
+        System.out.println(s.validUtf8(array1));
+
+        array1 = new int[]{230,136,145};
+        System.out.println(s.validUtf8(array1));
     }
 }
--- _393. UTF-8 Validation/src/Solution.java
+++ _393. UTF-8 Validation/src/Solution.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean validUtf8(int[] data) {
-        int[] prefixUTF8FistByte = new int[]{0b1100, 0b1110, 0b1111};
+
+        /* コードの i, UTFByteLengthの　依存性の解決する */
+
+        int[] prefixUTF8FistByte = new int[]{0b11000, 0b11100, 0b11110};
         final int prefixUTF8 = 0b10000000;
         final int OVER_UTF_BITS = 4;
         boolean isUTF8 = false;
@@ -16,25 +19,33 @@ class Solution {
             }
 
             int UTFByteLength = 0;
-            int firstBitUTF8 = data[i] >> 4;
+            int firstBitUTF8 = data[i] >> 3;
             while (UTFByteLength < prefixUTF8FistByte.length) {
                 if (firstBitUTF8 == prefixUTF8FistByte[UTFByteLength]) {
                     break;
                 }
                 UTFByteLength++;
             }
+            UTFByteLength++;
 
-            if (UTFByteLength == OVER_UTF_BITS) {
+            if (UTFByteLength >= OVER_UTF_BITS) {
                 break;
             }
 
             i++;
-            while (UTFByteLength >= 0 && i < data.length) {
-                int temp = data[i++] & prefixUTF8;
+            while (UTFByteLength > 0 && i < data.length) {
+                int temp = data[i] & prefixUTF8;
+                i++;
+
                 if (temp != prefixUTF8) {
                     break;
                 }
+
                 UTFByteLength--;
+            }
+
+            if (UTFByteLength <= 0) {
+                isUTF8 = true;
             }
         }
 
