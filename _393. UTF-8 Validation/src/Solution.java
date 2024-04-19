b97d39256d5b43c7a41851c25602a70a476eed88
Joo-hyun-Kang <autoba9687@gmail.com>
2024-04-19 19:23:53 +0900

    fix: change comparing logic of numbers of UTF

--- _393. UTF-8 Validation/src/Solution.java
+++ _393. UTF-8 Validation/src/Solution.java
@@ -1,54 +1,47 @@
 class Solution {
     public boolean validUtf8(int[] data) {
-
-        /* コードの i, UTFByteLengthの　依存性の解決する */
-
-        int[] prefixUTF8FistByte = new int[]{0b11000, 0b11100, 0b11110};
-        final int prefixUTF8 = 0b10000000;
-
         int i = 0;
         boolean isUTF8 = false;
         while (i < data.length) {
             isUTF8 = false;
 
             if (isAcsii(data[i])) {
-                System.out.printf("%d: %d¥n", i, data[i]);
-                
                 i++;
                 isUTF8 = true;
                 continue;
             }
 
-            int followedUTFBytes = -1;
-            int firstBitUTF8 = data[i++] >> 3;
-            for (int j = 0; j < prefixUTF8FistByte.length; j++) {
-                if (firstBitUTF8 == prefixUTF8FistByte[j]) {
-                    followedUTFBytes = j + 1;
-                    break;
-                }
-            }
+            int followedUTFBytes = GetUTFBytes(data[i]);
 
-            if (followedUTFBytes == -1) {
+            if (followedUTFBytes == -1 || followedUTFBytes > (data.length - 1) - i) {
                 break;
             }
 
             boolean isFollowedBytes = true;
+            final int prefixUTF8 = 0b10000000;
             for (int j = followedUTFBytes; j > 0; j--) {
-                int temp = data[i++] & prefixUTF8;
+                int temp = data[++i] & prefixUTF8;
 
                 if (temp != prefixUTF8) {
+                    isFollowedBytes = false;
+                    break;
+                }
+
+                temp = data[i] << 1 & prefixUTF8 << 1;
+
+                if (temp != 0b00000000) {
                     isFollowedBytes = false;
                     break;
                 }
             }
 
             if (isFollowedBytes) {
-                System.out.printf("%d: %d¥n", i - 1, data[i - 1]);
-                System.out.println();
                 isUTF8 = true;
             } else {
                 break;
             }
+
+            i++;
         }
 
         return isUTF8;
@@ -57,5 +50,45 @@ class Solution {
     private boolean isAcsii(int data) {
         final int temp = 0b10000000 & data;
         return temp == 0;
+    }
+
+    private int GetUTFBytes(int num) {
+        StringBuilder builder = new StringBuilder();
+
+        if (num == 0) {
+            return -1;
+        }
+
+        while (num > 0) {
+            if (num % 2 == 0) {
+                builder.append("0");
+            } else {
+                builder.append("1");
+            }
+
+            num /= 2;
+        }
+
+        // 1100
+        String value = builder.reverse().toString();
+        String[] prefixUTF8FistByte = {new String("110"), new String("1110"), new String("11110")};
+
+        int isUTF8 = -1;
+        for (int i = 0; i < prefixUTF8FistByte.length; i++) {
+            boolean isEqual = true;
+            for (int j = 0; j < prefixUTF8FistByte[i].length(); j++) {
+                if (value.charAt(j) != prefixUTF8FistByte[i].charAt(j)) {
+                    isEqual = false;
+                    break;
+                }
+            }
+
+            if (isEqual) {
+                isUTF8 = i + 1;
+                break;
+            }
+        }
+        
+        return isUTF8;
     }
 }
--- _Temp/src/Main.java
+++ _Temp/src/Main.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int temp = 0b1100_0000;
-        System.out.println(temp);
-
-        temp >>= 6;
-        System.out.println(temp);
+        System.out.println(0b100000 << 1);        
+        System.out.println(0b100000);
     }
 }
