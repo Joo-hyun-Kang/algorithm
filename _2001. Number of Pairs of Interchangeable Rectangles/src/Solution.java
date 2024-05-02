28eac2d456bd3e94f67e8acd7cc954f2033ade9d
Joo-hyun-Kang <autoba9687@gmail.com>
2024-05-03 03:17:51 +0900

    feat: first try using hashSet

--- _2001. Number of Pairs of Interchangeable Rectangles/src/Solution.java
+++ _2001. Number of Pairs of Interchangeable Rectangles/src/Solution.java
@@ -1,3 +1,21 @@
-public class Solution {
+import java.util.HashSet;
+
+class Solution {
+    public long interchangeableRectangles(int[][] rectangles) {
+        HashSet<Double> set = new HashSet<Double>();
+        long count = 0;
+
+        for (int i = 0; i < rectangles.length; ++i) {
+            double ratio = rectangles[i][0] / (double)rectangles[i][0];
+            
+            if (set.contains(ratio)) {
+                count++;
+            } else {
+                set.add(ratio);
+            }
+        }
+
+        return count;
+    }
 
 }
--- _2001. Number of Pairs of Interchangeable Rectangles/src/main.java
+++ _2001. Number of Pairs of Interchangeable Rectangles/src/main.java
@@ -1,6 +1,11 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Solution s = new Solution();
+        int[][] rectangles = new int[][]
+        {
+            {4,8},{3,6},{10,20},{15,30}
+        };
 
+        System.out.println(s.interchangeableRectangles(rectangles));
     }
 }
