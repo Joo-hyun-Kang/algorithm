148fc0623def7cc067cf21e7060eaa1ba5a6f737
Joo-hyun-Kang <autoba9687@gmail.com>
2024-05-08 15:13:38 +0900

    fix: adding fast solution for 2001

--- _2001. Number of Pairs of Interchangeable Rectangles/src/Solution.java
+++ _2001. Number of Pairs of Interchangeable Rectangles/src/Solution.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,4 +21,27 @@ public class Solution {
 
         return count;
     }
+
+//    public long interchangeableRectangles(int[][] rectangles) {
+//        int length = rectangles.length;
+//        double[] ratios = new double[length];
+//
+//        for (int i = 0; i < length; ++i) {
+//            ratios[i] = rectangles[i][0] / (double) rectangles[i][1];
+//        }
+//        Arrays.sort(ratios);
+//
+//        long curr = 0;
+//        long res = 0;
+//        for (int j = 0; j < length - 1; j++) {
+//            if (ratios[j] == ratios[j + 1]) {
+//                curr++;
+//                res += curr;
+//            } else {
+//                curr = 0;
+//            }
+//        }
+//
+//        return res;
+//    }
 }
